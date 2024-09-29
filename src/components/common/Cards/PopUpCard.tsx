a1e63630559e2410cfef6e0d1b6044a4264a992e
tomer-shavit <tomer.shavit5@gmail.com>
2024-09-29 21:04:14 +0300

    compiles

--- src/components/common/Cards/PopUpCard.tsx
+++ src/components/common/Cards/PopUpCard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { MdOutlineClose } from "react-icons/md";
 import ClickOutside from "@/components/ClickOutside";
@@ -10,22 +10,15 @@ interface PopUpCardProps {
 }
 
 const PopUpCard: React.FC<PopUpCardProps> = ({ children, onClose }) => {
-  const exceptionRef = useRef<HTMLDivElement>(null);
-
   return ReactDOM.createPortal(
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-60 px-4">
-      <ClickOutside exceptionRef={exceptionRef} onClick={onClose}>
+      <ClickOutside onClick={onClose}>
         <UpFade>
-          <div
-            ref={exceptionRef}
-            className="relative z-[10000]  mx-auto  w-full max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-gray-dark sm:max-w-3xl"
-          >
+          <div className="relative z-[10000] mx-auto w-full max-w-lg rounded-lg bg-white p-6 shadow-lg dark:bg-gray-dark sm:max-w-3xl">
             <MdOutlineClose
               onClick={onClose}
               className="absolute right-2 top-2 size-4 cursor-pointer text-gray-500 hover:text-gray-700 dark:text-white"
-            >
-              &times;
-            </MdOutlineClose>
+            />
             {children}
           </div>
         </UpFade>
